9f80a2c815dd06b4fe9caebd73bec5871e405367
Pratik Chaubey <pratikup98@gmail.com>
2025-08-10 21:30:23 +0530

    normal

--- node.js
+++ node.js
@@ -10,7 +10,41 @@ function add(a, b) {
     return a + b;
 }
 
+// Subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Divide two numbers
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Division by zero!";
+    }
+    return a / b;
+}
+
+// Check if a number is even or odd
+function checkEvenOdd(num) {
+    return num % 2 === 0 ? "Even" : "Odd";
+}
+
+// Reverse a string
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
 // Example usage
 let userName = "atul";
-console.log(greet(userName)); // Prints greeting
-console.log("Sum of 5 and 10 is:", add(5, 10)); // Prints sum
+console.log(greet(userName)); 
+
+console.log("Sum of 5 and 10 is:", add(5, 10)); 
+console.log("Difference of 10 and 3 is:", subtract(10, 3));
+console.log("Product of 4 and 6 is:", multiply(4, 6));
+console.log("Division of 20 by 4 is:", divide(20, 4));
+console.log("Check if 7 is even or odd:", checkEvenOdd(7));
+console.log("Reverse of 'JavaScript' is:", reverseString("JavaScript"));
